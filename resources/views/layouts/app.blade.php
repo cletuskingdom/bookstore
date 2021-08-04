5cda2d526fb9faa7317f1da6e7dac5ff32c63001
cletuskingdom <cletuskingdom@gmail.com>
2021-08-04 14:33:28 +0100

    updated the fron-end and added a custom book image.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,14 +3,15 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/app.js') }}" ></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -19,12 +20,13 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
+
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark shadow-sm fixed-top scrolling-navbar">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    <img src="{{url('storage/logo.png')}}" alt="{{ config('app.name') }}" class="img-fluid" id="logo">
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -38,14 +40,20 @@
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ url('/') }}"> Home </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#!"> About us</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#!"> Contact us</a>
+                        </li>
                         <!-- Authentication Links -->
                         @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
-
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
                             @if (Route::has('register'))
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
@@ -54,30 +62,117 @@
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
+                                    {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="/author/dashboard">
+                                       Profile
+                                    </a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                         @csrf
                                     </form>
                                 </div>
                             </li>
                         @endguest
+                        
                     </ul>
                 </div>
             </div>
         </nav>
 
-        <main class="py-4">
+        <main>
             @yield('content')
         </main>
+
+        <footer>
+            <div class="text-center bg-dark py-3 shadow-lg text-light">
+
+                <div class="container pt-4">
+                    <div class="row">
+                        <div class="col-md-4 mb-4 py-2">
+                            <h4>{{ config('app.name') }}</h4>
+
+                            <article class="pt-2">
+                                <b>{{ config('app.name') }}</b> is a
+                                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                            </article>
+                        </div>
+
+                        <div class="col-md-4 mb-4 py-2">
+                            <h4>Useful links</h4>
+
+                            <ul class="navbar-nav ml-auto pt-2">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ url('/') }}">
+                                        Home
+                                    </a>
+                                </li>
+
+                                <li class="nav-item">
+                                    <a class="nav-link" href="">{{ __('How it works') }}</a>
+                                </li>
+                            </ul>
+                        </div>
+
+                        <div class="col-md-4 mb-4 py-2">
+                            <h4>Contact us</h4>
+
+                            <article class="pt-2">
+                                
+                                <ul class="navbar-nav ml-auto pt-2">
+                                    <li class="nav-item">
+                                        <i class="fa fa-map-marker pr-2"></i>
+                                        7 factory Road Aba, Abia State, Nigeria.
+                                    </li>                                
+
+                                    <li class="nav-item">
+                                        <a href="tel:07063964065" class="nav-link"><i class="fa fa-phone pr-2" aria-hidden="true"></i> Call Now - 07063964065</a>
+                                    </li>
+
+                                    <li class="nav-item">
+                                        <a href="tel:07063964065" class="nav-link"><i class="fa fa-phone pr-2" aria-hidden="true"></i> Call Now - 07063964065</a>
+                                    </li>    
+                                </ul>
+
+                                <div class="nav justify-content-center">
+                                    <a href="" class="nav-link" target="_blank"><i class="fab fa-facebook pr-2" aria-hidden="true"></i> </a>
+                                    <a href="" class="nav-link" target="_blank"><i class="fab fa-instagram pr-2" aria-hidden="true"></i> </a>
+                                    <a href="" class="nav-link" target="_blank"><i class="fab fa-twitter pr-2" aria-hidden="true"></i> </a>
+                                </div>
+                            </article>
+                        </div>
+                    </div>
+                </div>
+
+                <hr>
+
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-6 pt-3">
+                            <p> <b>{{ config('app.name') }}.</b> &#169; Copyright <?=date('Y')?> </p>
+                        </div>
+    
+                        <div class="col-md-6 pt-2">
+                            <div class="d-flex justify-content-around">
+                                <a href="" class="nav-link">Terms and conditions</a>
+                                <a href="" class="nav-link">Privacy policy</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
     </div>
 </body>
 </html>
